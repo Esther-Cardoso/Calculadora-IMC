7a42c08f4efa10de96f3979246040174172dbb5d
Esther Cardoso <70102263+Esther-Cardoso@users.noreply.github.com>
2022-01-26 18:10:54 +0000

    Adicionando classificação no box resultado

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ function imc() {
   const resultado = document.querySelector('#resultado');
 
   if(altura !== '' && peso !== '') {
-    let soma = (peso / (altura * altura)).toFixed(2);
-    resultado.textContent = 'Seu IMC: ' + soma;
+    const soma = (peso / (altura * altura)).toFixed(2);
+
+    let classificacao = '';
+
+    if (soma < 18.5) {
+      classificacao = 'abaixo do peso (Magreza)';
+    } else if (soma < 25) {
+      classificacao = 'com o peso ideal (Normal)';
+    } else if (soma < 30) {
+      classificacao = 'acima do peso (Sobrepeso grau I)';
+    } else if (soma < 40) {
+      classificacao = 'com obesidade (obesidade grau II)';
+    } else {
+      classificacao = 'com obesidade grave (obesidade grau III)';
+    }
+
+    resultado.textContent = `IMC: ${soma}. Você está ${classificacao}`;
+
   } else {
     resultado.textContent = 'Preencha todos os campos!!!';
   }
